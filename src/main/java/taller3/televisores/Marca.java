feabe1a0046afbfa922f61011b9067c881a402c9
OscarArango1 <oarangog@unal.edu.co>
2024-11-16 19:36:30 -0500

    Pls

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,16 @@
+package taller3.televisores;
+
+public class Marca {
+    private String nombre;
+    public Marca (String name){
+        this.nombre = name;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String name){
+        this.nombre = name;
+    }
+}
